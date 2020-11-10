326efba3aba34784abaf5fb03a57f885bbf0c77e
geyingli <luv_dusk@163.com>
2020-11-10 17:32:49 +0800

    beta v2.2.3

--- uf/application/sanet.py
+++ uf/application/sanet.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-''' Applications based on BERT-based Retro-Reader. '''
+''' Applications based on SANet. '''
 
 import numpy as np
 

b0c44e2799c07a826125eda7fd29e20a5e0094d6
geyingli <geyingli@tencent.com>
2022-10-19 09:17:25 +0800

    v2.5.5

--- uf/apps/rnn/rnn.py
+++ uf/apps/rnn/rnn.py
@@ -1,12 +1,7 @@
-import math
-import copy
-import json
-import random
-import collections
-
 from .._base_._base_ import BaseEncoder
 from .. import util
 from ...third import tf
+from tf.contrib.rnn import RNNCell, LSTMCell, GRUCell, DropoutWrapper, MultiRNNCell
 
 
 class RNNEncoder(BaseEncoder):
@@ -14,7 +9,9 @@ class RNNEncoder(BaseEncoder):
         self,
         is_training,
         input_ids,
+        seq_length,
         vocab_size,
+        rnn_core="lstm",
         hidden_size=128,
         scope="rnn",
         initializer_range=0.02,
@@ -27,7 +24,9 @@ class RNNEncoder(BaseEncoder):
         max_seq_length = input_shape[1]
 
         with tf.variable_scope(scope):
-            embedding_output, embedding_table = util.embedding_lookup(
+
+            # embedding
+            embedding_output, _ = util.embedding_lookup(
                 input_ids=input_ids,
                 vocab_size=vocab_size,
                 batch_size=batch_size,
@@ -38,4 +37,19 @@ class RNNEncoder(BaseEncoder):
                 tilda_embeddings=kwargs.get("tilda_embeddings"),
                 trainable=trainable,
             )
-            
+
+            # backbone
+            if rnn_core == "rnn":
+                cell = RNNCell(num_units=hidden_size, trainable=trainable)
+            elif rnn_core == "lstm":
+                cell = LSTMCell(num_units=hidden_size, trainable=trainable)
+            elif rnn_core == "gru":
+                cell = GRUCell(num_units=hidden_size, trainable=trainable)
+            outputs, last_states = tf.nn.dynamic_rnn(
+                inputs=embedding_output,
+                cell=cell,
+                dtype=tf.float64,
+                sequence_length=seq_length)
+
+        print(outputs)
+        print(last_states)
--- uf/apps/rnn/rnn_classifier.py
+++ uf/apps/rnn/rnn_classifier.py
@@ -61,8 +61,9 @@ class RNNClassifier(BERTClassifier, ClassifierModule):
         # convert X
         if X is not None or X_tokenized is not None:
             tokenized = False if X is not None else X_tokenized
-            input_ids = self._convert_X(X_tokenized if tokenized else X, tokenized=tokenized)
+            input_ids, seq_length = self._convert_X(X_tokenized if tokenized else X, tokenized=tokenized)
             data["input_ids"] = np.array(input_ids, dtype=np.int32)
+            data["seq_length"] = np.array(seq_length, dtype=np.int32)
             n_inputs = len(input_ids)
 
             if n_inputs < self.batch_size:
@@ -91,33 +92,48 @@ class RNNClassifier(BERTClassifier, ClassifierModule):
                 raise ValueError("Wrong input format (line %d): \"%s\". " % (idx, sample))
 
         input_ids = []
+        seq_length = []
         for idx, segments in enumerate(segment_input_tokens):
             _input_tokens = []
             _input_ids = []
+            _seq_length = 0
 
             com.truncate_segments(segments, self.max_seq_length - len(segments), truncate_method=self.truncate_method)
             for segment in segments:
                 _input_tokens.extend(segment + ["[SEP]"])
 
             _input_ids = self.tokenizer.convert_tokens_to_ids(_input_tokens)
+            _seq_length = len(_input_ids)
 
             # padding
             _input_ids = [0] * (self.max_seq_length - len(_input_ids)) + _input_ids
 
             input_ids.append(_input_ids)
+            seq_length.append(_seq_length)
 
-        return input_ids
+        return input_ids, seq_length
 
     def _set_placeholders(self, **kwargs):
         self.placeholders = {
             "input_ids": tf.placeholder(tf.int32, [None, self.max_seq_length], "input_ids"),
+            "seq_length": tf.placeholder(tf.int32, [None], "seq_length"),
             "label_ids": tf.placeholder(tf.int32, [None], "label_ids"),
             "sample_weight": tf.placeholder(tf.float32, [None], "sample_weight"),
         }
 
     def _forward(self, is_training, placeholders, **kwargs):
 
-        encoder = RNNEncoder()
+        encoder = RNNEncoder(
+            is_training=is_training,
+            input_ids=placeholders["input_ids"],
+            seq_length=placeholders["seq_length"],
+            vocab_size=self.tokenizer.vocab,
+            rnn_core=self._rnn_core,
+            hidden_size=self._hidden_size,
+            scope=self._rnn_core,
+            trainable=True,
+            **kwargs,
+        )
         encoder_output = encoder.get_pooled_output()
         decoder = ClsDecoder(
             is_training=is_training,

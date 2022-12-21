99f1382372939d3d9125b537433ecf3ec1398746
geyingli <geyingli@tencent.com>
2022-12-21 23:27:35 +0800

    v2.5.19

--- uf/apps/__init__.py
+++ uf/apps/__init__.py
@@ -22,6 +22,7 @@ from .widedeep.widedeep_classifier import WideDeepClassifier
 from .sembert.sembert_classifier import SemBERTClassifier
 from .performer.performer_classifier import PerformerClassifier
 from .uda.uda_classifier import UDAClassifier
+from .motian.motian_classifier import MotianClassifier
 from .tinybert.tinybert_classifier import TinyBERTClassifier
 from .tinybert.tinybert_binary_classifier import TinyBERTBinaryClassifier
 from .fastbert.fastbert_classifier import FastBERTClassifier
@@ -132,4 +133,5 @@ __all__ = [
     "AdaBERTClassifier",
     "ChatbotMT",
     "UniLMPrompt",
+    "MotianClassifier",
 ]
